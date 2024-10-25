08396f406d31885a65776ea586714df7d3e88a80
Cheng Li <lichengac@gmail.com>
2024-10-25 18:24:37 -0400

    Game over and complete scene

--- scences/buttons/feather.gd
+++ scences/buttons/feather.gd
@@ -4,12 +4,45 @@ signal clicked
 
 @export var key := ''
 
+@export var color := 'white'
+
+var textures = {
+	"white": {
+		"light": "res://assets/images/buttons/feather_white.png",
+		"dark":  "res://assets/images/buttons/feather_grey.png",
+		"font_color": "#ff8d3a",
+		"font_shadow": "#77422c",
+	},
+	"red": {
+		"light": "res://assets/images/buttons/feather_red.png",
+		"dark": "res://assets/images/buttons/feather_red_dark.png",
+		"font_color": "#ffffff",
+		"font_shadow": "#666666",
+	},
+	"yellow": {
+		"light": "res://assets/images/buttons/feather_yellow.png",
+		"dark": "res://assets/images/buttons/feather_yellow_dark.png",
+		"font_color": "#c40079",
+		"font_shadow": "#641a2a",
+	},
+}
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	$TextureButton.connect("button_down", self._on_button_down)
 	$TextureButton.connect("button_up", self._on_button_up)
 	$TextureButton/Label.text = tr(key)
-	pass # Replace with function body.
+	
+	var light_image = Image.load_from_file(textures[color].light)
+	var light_texture = ImageTexture.create_from_image(light_image)
+	var dark_image = Image.load_from_file(textures[color].dark)
+	var dark_texture = ImageTexture.create_from_image(dark_image)
+	
+	$TextureButton.texture_normal = dark_texture
+	$TextureButton.texture_hover = light_texture
+	
+	$TextureButton/Label.add_theme_color_override("font_color", textures[color].font_color);
+	$TextureButton/Label.add_theme_color_override("font_shadow_color", textures[color].font_shadow);
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
