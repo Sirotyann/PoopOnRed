26c00537869128cfd6d454618a83ed58204867b3
Cheng Li <lichengac@gmail.com>
2024-10-31 18:28:42 -0400

    loading

--- scenes/general/completed.gd
+++ scenes/general/completed.gd
@@ -3,8 +3,11 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	play_audio()
+	$AudioStreamPlayer.connect("finished", self.play_audio)
 
+func play_audio():
+	$AudioStreamPlayer.play()
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
--- scenes/general/loading.gd
+++ scenes/general/loading.gd
@@ -3,10 +3,14 @@ extends Node2D
 var progress = []
 var target_scence = null
 var scene_load_status = 0
+
+var bird_init_x := 50.0
  
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	$AnimatedSprite2D.play("fly")
+	var size = get_viewport_rect().size
+	print(size)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
@@ -20,6 +24,9 @@ func _process(delta: float) -> void:
 
 func update_progress(val):
 	$Percentage.text = "{progress}%".format({"progress": val * 100})
+	var size = get_viewport_rect().size
+	var x = bird_init_x + (size[0] - 2*bird_init_x) * val
+	$AnimatedSprite2D.position.x = x
 
 func switch_scence(path):
 	target_scence = path
