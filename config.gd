de55ccf0670526a4d6976ee8c6b4ae71e1e2d8b7
Cheng Li <lichengac@gmail.com>
2024-11-18 18:25:12 -0500

    fix

--- config.gd
+++ config.gd
@@ -2,10 +2,10 @@ extends Node
 
 class_name Config
 
-const is_dev := true # true  false
+const is_dev := false # true  false
 
 const mode := "PC" # MOBILE / PC
 const device := "PC" # iPhone / PC
 
-const EnoughPracticeCount := 2
+const EnoughPracticeCount := 5
 const MaxLife := 3
